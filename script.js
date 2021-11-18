44a94204a54ee8a40149e213d9b6e93e87c02f94
Fernando Ponce Solís <poncefernando674@gmail.com>
2021-11-17 22:13:39 -0700

    New things with jQuery, so slow but that is what my teacher wanted

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 $(document).ready(() => {
-    alert("Bienvenido!!")
+    alert('Bienvenido!!');
+    $('.nombre').text('Fernando Ponce Solís');
+    $('.nombre').css('color', 'white');
+    $('.nombre').css('background-color', 'rgb(85,115,248)');
+    $('.nombre').css('text-align', 'center');
+    $('.nombre').css('margin', '0');
+    if ($('.nombre').hasClass('saludo')){
+            $('.nombre').show(3000);
+        }
+});
+
+$('.nombre').click(() => {
+    $('.nombre').hide(300);
 });
 
 function mover_a() {
-    location.href = "https://google.com";
+    location.href = 'https://google.com';
 }
 
